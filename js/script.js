85740b8efedc4809f46b3311ac90db2519946599
Cix8 <francesco.natal_fn@libero.it>
2022-05-09 14:19:17 +0200

    add milestone1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const exampleTextArray = ['Fare la spesa', 'Pulire casa', 'Terminare gli esercizi'];
+class Todo {
+    constructor(text, done) {
+        this.text = text,
+        this.done = done
+    }
+}
+
+const objArray = [];
+
+for (let i = 0; i < exampleTextArray.length; i++) {
+    if (i === 1) {
+        objArray.push(new Todo(exampleTextArray[i], true));    
+    } else {
+        objArray.push(new Todo(exampleTextArray[i], false));    
+    }
+}
+
+console.log(objArray);
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            mainData: objArray
+        }
+    }
+)
